fdaab75aca81d7a1c514f7efd87888ec31e1e657
Lakshay Agarwal <633lakshay@gmail.com>
2023-10-17 23:39:55 +0530

    Add files via upload

---
+++ PointerP1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num = 5;
+    cout <<"the address of num storage is "<<&num<<endl;   // address of operator :- &
+    
+    int *ptr = &num;
+    cout << "*ptr = "<<*ptr<<endl;      // derefernce operator :- *
+    cout << "ptr = "<<ptr<<endl; 
+    cout << "&ptr = "<<&ptr<<endl; 
+    cout << "size of num = "<<sizeof(num)<<endl; 
+    cout << "size of ptr = "<<sizeof(ptr)<<endl;
+
+    double d = 11.1;
+    double *p2 = &d;
+    cout << "size of d = "<<sizeof(d)<<endl; 
+    cout << "size of p2 = "<<sizeof(p2)<<endl;  // hence size of ptr is constant ... here 4, can also be 8
+
+    return 0;
+}
