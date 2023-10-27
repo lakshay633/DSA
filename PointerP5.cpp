0c2aa6a243c21be4d3b432009aa45d17b586a38f
Lakshay Agarwal <633lakshay@gmail.com>
2023-10-27 21:28:39 +0530

    Add files via upload

---
+++ PointerP5.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+void update(int ** q){
+    // q+=1;       //just the copy will change
+    // *q+=1;      // value of p will increment
+    **q+=1;     // value of num will increment
+}
+int main()
+{
+    int num = 5;
+    int* p=&num;
+    int** q=&p;
+    
+    // ways to print value of num
+    cout<<num<<endl;
+    cout<<*p<<endl;
+    cout<<**q<<endl<<endl;
+
+    // ways to print address of num
+    cout << & num<<endl;
+    cout <<p<<endl;
+    cout <<*q<<endl<<endl;
+    
+    // ways to print address of p
+    cout << &p<<endl;
+    cout <<q<<endl<<endl;
+
+    // ways to print address of q
+    cout << &q<<endl<<endl;
+
+    cout <<"before function"<<endl;
+    cout <<num<<endl;
+    cout <<p<<endl;
+    cout <<q<<endl;
+    update(q);
+    cout <<"after function"<<endl;
+    cout <<num<<endl;
+    cout <<p<<endl;
+    cout <<q<<endl;
+    return 0;
+}
